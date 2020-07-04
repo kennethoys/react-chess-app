9b752e26ad8b5e2eaeb24d3f6eb7e254549a2400
Kenneth <general.kenneth@hotmail.com>
2020-07-04 13:16:39 +0800

    Updates

---
+++ precache-manifest.d9620e57a616ade2749d43692b6187d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7713783e9ee5a924b825a80e58d86c",
+    "url": "/react-chess-app/index.html"
+  },
+  {
+    "revision": "b4c93d667d9f7793d91b",
+    "url": "/react-chess-app/static/css/main.5e2e3255.chunk.css"
+  },
+  {
+    "revision": "38190722cca580595671",
+    "url": "/react-chess-app/static/js/2.6f258c37.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-chess-app/static/js/2.6f258c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c93d667d9f7793d91b",
+    "url": "/react-chess-app/static/js/main.0b67a242.chunk.js"
+  },
+  {
+    "revision": "762446ee13ebc5c9bde1",
+    "url": "/react-chess-app/static/js/runtime-main.1f4a976a.js"
+  }
+]);
